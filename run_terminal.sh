de16875b0bdef4cf5ed87664efc599b5b3d1e789
Manje Brinkhuis <manjebrinkhuis@gmail.com>
2018-05-21 22:09:09 +0000

    Bugfixes

--- run_terminal.sh
+++ run_terminal.sh
@@ -1,9 +1,13 @@
+# Create conda volume
+# docker volume create neuro
+
+#
 docker run --rm \
     -it \
     -p 8888:8888 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
-    -v /mnt/data:/data \
+    -v /mnt:/data \
     -v /mnt/data/Apps/Linux/freesurfer:/opt/freesurfer \
     --name neuro-docker \
     manjebrinkhuis/neuro-docker
